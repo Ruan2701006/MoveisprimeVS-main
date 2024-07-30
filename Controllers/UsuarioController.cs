a9bd2274917348afb54aa3c121426a138b6a3e26
kbnotek <ruan.polovina@aluno.senai.br>
2024-07-30 16:33:59 -0300

    22

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using MoveisprimeVS.Models;
+using System.Diagnostics;
+
+namespace MoveisprimeVS.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly ILogger<UsuarioController> _logger;
+
+        public UsuarioController(ILogger<UsuarioController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
